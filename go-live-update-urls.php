412147aea1d0e13a1cc3e68c9fa4e4099cac6069
Mat Lipe <Mat Lipe@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-10 23:44:04 +0000

    version 2.4.5
    git-svn-id: https://plugins.svn.wordpress.org/go-live-update-urls/trunk@986850 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- go-live-update-urls.php
+++ go-live-update-urls.php
@@ -5,7 +5,7 @@ Plugin URI: http://matlipe.com/go-live-update-urls/
 Description: This Plugin Updates all the URLs in the database to point to the new URL when making your site live or changing domains.
 Author: Mat Lipe
 Author URI: http://matlipe.com/
-Version: 2.4.4
+Version: 2.4.5
 */
 
 define( 'GLUU_VIEWS_DIR', plugin_dir_path(__FILE__) . 'views/' );
